605d7bc06c2f71a26560486319077f383d054c22
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2025-02-09 02:33:48 +0100

    feat: add csrf synchronizer pattern

--- src/main/java/org/example/marketplacebackend/config/SecurityConfig.java
+++ src/main/java/org/example/marketplacebackend/config/SecurityConfig.java
@@ -1,33 +1,67 @@
 package org.example.marketplacebackend.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.function.Supplier;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 @EnableWebSecurity
-// @EnableMethodSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+  @Value("${ALLOWED_ORIGINS:http://localhost:3000}")
+  private String allowedOrigins;
+
+  @Value("${CSRF_DOMAIN:}")
+  private String csrfDomain;
+
+  @Value("${CSRF_SAME_SITE:Lax}")
+  private String csrfSameSite;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.cors(Customizer.withDefaults());
 
-    // TODO: fix later
-    http.csrf(AbstractHttpConfigurer::disable);
+    CookieCsrfTokenRepository csrfRepo = CookieCsrfTokenRepository.withHttpOnlyFalse();
+    csrfRepo.setCookieCustomizer(cookieCsrfToken -> {
+      cookieCsrfToken.sameSite(csrfSameSite);
+      cookieCsrfToken.domain(csrfDomain);
+    });
+
+    http.csrf(csrf -> csrf
+            .csrfTokenRepository(csrfRepo)
+            .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+        )
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
 
     http.authorizeHttpRequests(auth -> auth
         // allow OPTION requests
@@ -84,25 +118,67 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
-  // TODO: Investigate why CORS credentials can't be configured selectively:
-  //  When only allowing credentials for login/logout globally,
-  //  it seems to override the class level CORS configurations in controllers.
-  //  This results in credentials not being allowed in any other endpoints than login/logout.
-  //  As such, the following is a workaround which simply allows credentials for everything.
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration withCreds = new CorsConfiguration();
-    withCreds.setAllowedOrigins(List.of("http://localhost:3000", "https://marketplace.johros.dev"));
-    withCreds.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-    withCreds.setAllowCredentials(true);
-    withCreds.setAllowedHeaders(List.of("Content-Type"));
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    corsConfig.setAllowCredentials(true);
+    corsConfig.setAllowedHeaders(List.of("Content-Type", "X-XSRF-TOKEN"));
 
     UrlBasedCorsConfigurationSource config = new UrlBasedCorsConfigurationSource();
-    config.registerCorsConfiguration("/**", withCreds);
-    config.registerCorsConfiguration("/v1/accounts/login", withCreds);
-    config.registerCorsConfiguration("/v1/accounts/logout", withCreds);
+    config.registerCorsConfiguration("/**", corsConfig);
 
     return config;
   }
 
+}
+
+final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
+
+  private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
+
+  @Override
+  public void handle(HttpServletRequest request, HttpServletResponse response,
+      Supplier<CsrfToken> csrfToken) {
+    /*
+     * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
+     * the CsrfToken when it is rendered in the response body.
+     */
+    this.delegate.handle(request, response, csrfToken);
+  }
+
+  @Override
+  public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
+    /*
+     * If the request contains a request header, use CsrfTokenRequestAttributeHandler
+     * to resolve the CsrfToken. This applies when a single-page application includes
+     * the header value automatically, which was obtained via a cookie containing the
+     * raw CsrfToken.
+     */
+    if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
+      return super.resolveCsrfTokenValue(request, csrfToken);
+    }
+    /*
+     * In all other cases (e.g. if the request contains a request parameter), use
+     * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
+     * when a server-side rendered form includes the _csrf request parameter as a
+     * hidden input.
+     */
+    return this.delegate.resolveCsrfTokenValue(request, csrfToken);
+  }
+}
+
+final class CsrfCookieFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
+      FilterChain filterChain)
+      throws ServletException, IOException {
+    CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+    // Render the token value to a cookie by causing the deferred token to be loaded
+    csrfToken.getToken();
+
+    filterChain.doFilter(request, response);
+  }
 }
