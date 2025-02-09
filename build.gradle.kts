f1f71a7f140f4df1cb1c7180abcdff13ddba0029
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2025-02-09 18:19:56 +0100

    chore: upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
@@ -22,17 +22,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:postgresql")
-    compileOnly("org.projectlombok:lombok")
-    runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.4.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.2")
+    testImplementation("org.testcontainers:junit-jupiter:1.20.4")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers:3.4.2")
+    testImplementation("org.testcontainers:postgresql:1.20.4")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    runtimeOnly("org.postgresql:postgresql:42.7.5")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.2")
+    testImplementation("org.springframework.security:spring-security-test:6.4.2")
 }
 
 tasks.withType<Test> {
